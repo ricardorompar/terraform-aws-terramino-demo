5d45b33bb80ead2989c09d6ec8d7cd8e7a766917
ricardorompar <ricardo.romero@hashicorp.com>
2025-02-04 10:04:08 +0100

    added project

--- assets/setup-hcp-terraform-initial/main.tf
+++ assets/setup-hcp-terraform-initial/main.tf
@@ -13,8 +13,15 @@ data "tfe_organization" "org" {
   name = var.tfc_org
 }
 
-resource "tfe_workspace" "hashicat" {
+resource "tfe_project" "acme" {
+  name = "acme-project"
+  organization = data.tfe_organization.org.name
+  description = "Project for ACME infrastructure."
+}
+
+resource "tfe_workspace" "terramino" {
   name           = "terramino-${var.admin_user}"
   organization   = data.tfe_organization.org.name
   tag_names      = var.tf_workspace_tags
+  project_id = tfe_project.acme.id
 }
