1c9ae98dca44ea68d21138ff01042a737751ceb5
ricardorompar <ricardo.romero@hashicorp.com>
2025-02-09 21:28:56 +0100

    changed vm sizes

--- azure/variables.tf
+++ azure/variables.tf
@@ -12,8 +12,8 @@ variable "instance_type" {
   type    = string
   default = "Standard_B1s"
   validation {
-    condition     = contains(["Standard_B1s", "Standard_B2s", "Standard_B2ps_v2"], var.instance_type)
-    error_message = "Valid instance types are Standard_B1s, Standard_B2s, and Standard_B2ps_v2."
+    condition     = contains(["Standard_B1s", "Standard_B1ms", "Standard_B2s", "Standard_B2ps_v2"], var.instance_type)
+    error_message = "Valid instance types are Standard_B1s, Standard_B1ms, Standard_B2s, and Standard_B2ps_v2."
   }
 }
 
